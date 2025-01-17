025420c21e006d8f6220d23be2201e442b7f5551
tuzicomet <leo336570@gmail.com>
2025-01-18 04:40:59 +1300

    PostCSS configuration file added
    PostCSS configuration file addd with tailwind and autoprefixer added. Code from the tailwindcss installation guide (using postcss)
    
    (Forgot to add this earlier)

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+//PostCSS configuration file
+
+module.exports = {
+    // plugins to process CSS with
+    plugins: {
+        tailwindcss: {},  // adds tailwind CSS utilities
+        autoprefixer: {}, // adds vendor prefixes for better browser support
+    },
+};
